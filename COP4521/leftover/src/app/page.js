dce91c3bfdc8ebd7ff74071340cb1063063dfd75
Brady Henderson <bradyhendu@gmail.com>
2024-01-24 16:36:09 -0500

    Creates the landing page

--- COP4521/leftover/src/app/page.js
+++ COP4521/leftover/src/app/page.js
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>
-
+    <div className="w-full h-screen flex items-center justify-center">
+      <div>
+        <div className="container mx-auto flex flex-col lg:flex-row lg:gap-2 gap-6 space-x-extra">
+          <div className="flex items-center justify-center flex-col gap-y-8 mt-8 lg:mt-0">
+            <h1 className="text-5xl font-bold text-primary">Do you want an easy way to find recipies with your food in your pantry?</h1>
+            <p className="text-xl">Leftover gives our users the opportunity to digitally tack all of the food they have in their pantries and find recipies that will use the food that they have. 
+              We also track expiration dates and will notify you when your food is about to expire so you can cook it before it goes bad.
+              Also, Chef's can post their own recipies and have them be searchable by our users. 
+            </p>
+            <Link href="/register">
+            <button className="bg-primary hover:bg-secondary hover:text-primary text-muted font-bold py-2 px-4 rounded">
+                Get Started Today!
+            </button>
+            </Link>
+          </div>
+          <Image src="/pantry.jpg" width={400} height={300} />
+        </div>
+      </div>
     </div>
   );
 }
