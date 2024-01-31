cd833285c618e4f8ac7b9a5a167dd404b73d630c
Brady Henderson <bradyhendu@gmail.com>
2024-01-30 22:27:04 -0500

    Starting point for the registration page, needs some styling fixes

--- COP4521/leftover/src/app/register/page.js
+++ COP4521/leftover/src/app/register/page.js
@@ -1,7 +1,53 @@
+import Image from "next/image";
+
 export default function Register() {
     return (
-      <div>
-        Registration page
+      <div className="flex h-screen">
+        <div className="w-1/2 flex items-center justify-center">
+          <form className="w-1/2 bg-primary flex flex-col rounded-lg shadow-2xl space-y-2 text-left p-6">
+            <h1 className="text-3xl font-signature mb-6 text-muted">Create your Account</h1>
+            <label className="font-bold text-lg text-muted">First Name</label>
+            <input
+              className="border border-accent rounded-lg px-2 py-2"
+              type="text"
+              placeholder="First Name"
+            />
+            <label className="font-bold text-lg text-muted">Last Name</label>
+            <input
+              className="border border-accent rounded-lg px-2 py-2"
+              type="text"
+              placeholder="Last Name"
+            />
+            <label className="font-bold text-lg text-muted">Email</label>
+            <input
+              className="border border-accent rounded-lg px-2 py-2"
+              type="text"
+              placeholder="Email"
+            />
+            <label className="font-bold text-lg text-muted">Username</label>
+            <input
+              className="border border-accent rounded-lg px-2 py-2"
+              type="text"
+              placeholder="Username"
+            />
+            <label className="font-bold text-lg text-muted">Password</label>
+            <input
+              className="border border-accent rounded-lg px-2 py-2"
+              type="password"
+              placeholder="Password"
+            />
+            <label className="font-bold text-lg text-muted">Confirm Password</label>
+            <input
+              className="border border-accent rounded-lg px-2 py-2 mb-4"
+              type="password"
+              placeholder="Confirm Password"
+            />
+            <button className="bg-secondary hover:bg-muted text-white hover:text-secondary rounded-lg py-2">
+              Create Account
+            </button>
+          </form>
+        </div>
+        <Image src="/registration.jpg" width={400} height={400} className="w-1/2 h-full"/>
       </div>
     );
   }
