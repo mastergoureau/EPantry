5b85e1d1cb81f9b23917cb55517aff03f3c1a4d8
Brady Henderson <bradyhendu@gmail.com>
2024-02-08 10:41:06 -0500

    Fixes typo on the home page

--- COP4521/leftover/src/app/page.js
+++ COP4521/leftover/src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="w-full flex items-center justify-center">
         <div className="w-1/2 flex items-center justify-center flex-col gap-y-8 mt-20 mx-4">
             <h1 className="text-5xl font-bold text-primary">Do you want an easy way to find recipies with the food <span className="text-secondary">LeftOver </span>in your pantry?</h1>
-            <p className="text-xl">Leftover gives our users the opportunity to digitally tack all of the food they have in their pantries and find recipies that will use the food that they have. 
+            <p className="text-xl">Leftover gives our users the opportunity to digitally track all of the food they have in their pantries and find recipies that will use the food that they have. 
               We also track expiration dates and will notify you when your food is about to expire so you can cook it before it goes bad.
               Also, Chef's can post their own recipies and have them be searchable by our users. 
             </p>
