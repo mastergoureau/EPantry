02f2439dfa86589f82131a236d8143dfd90d782e
Brady Henderson <bradyhendu@gmail.com>
2024-03-11 12:12:23 -0400

    flask backend test

---
+++ COP4521/leftover/src/app/flask/page.js
@@ -0,0 +1,29 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+
+const flask = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/data');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+        <h1>Flask</h1>
+        <p>API response: {data ? data.message : 'Loading...'}</p>
+    </div>
+  );
+};
+
+export default flask;
